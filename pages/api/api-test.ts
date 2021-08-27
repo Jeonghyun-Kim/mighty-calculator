747fd92e9f0cb166b993c50bf8e640b43e31ad4c
Kay Kim <kimjh@bawi.org>
2021-08-27 18:37:40 +0900

    test2

--- pages/api/api-test.ts
+++ pages/api/api-test.ts
@@ -4,7 +4,7 @@ import got from 'got';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'GET') {
-    await got('http://3.38.9.149:3001/v1/trade');
+    await got('http://3.38.9.149:3001/v1/trade', { headers: { Authorization: 'jongsik' } });
 
     return res.json({ hello: 'world' });
   }
